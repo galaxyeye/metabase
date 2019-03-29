e0b90d53bc2a0ee7d3960522531248449a447ab4
Vincent Zhang <ivincent.zhang@gmail.com>
2019-03-29 14:29:53 +0800

    Improve X-SQL setup procedure

--- src/metabase/driver/h2tcp.clj
+++ src/metabase/driver/h2tcp.clj
@@ -3,25 +3,28 @@
             [honeysql.core :as hsql]
             [metabase.driver.h2 :as h2]
             [metabase
-             [db :as mdb]
              [driver :as driver]
              [util :as u]]
-            [metabase.db.spec :as dbspec]
             [metabase.driver.generic-sql :as sql]
             [metabase.models.database :refer [Database]]
-            [metabase.util.honeysql-extensions :as hx]
-            [toucan.db :as db])
+            [clojure.java.jdbc :as jdbc])
   (:import (ai.platon.pulsar.ql H2Config)))
 
 (defn- connection-details->spec
   "Create a database specification for a h2 database."
-  [{:keys [host port db]
-    :or {host "localhost", port 9092, db "x-sql-test"}
-    :as opts}]
-  (merge {:classname "org.h2.Driver" ; must be in classpath
+  [{:keys [host port dbname]
+    :or   {host "localhost", port 9092, dbname "xsqltest"}
+    :as   opts}]
+  (merge {:classname   "org.h2.Driver"                      ; must be in classpath
           :subprotocol "h2"
-          :subname (str "tcp://" host ":" port "/~/" db)}
-         (dissoc opts :host :port :db)))
+          :subname     (str "tcp://" host ":" port "/~/" dbname)
+          :user "sa"
+          :password "sa"}
+         (dissoc opts :host :port :dbname :user :password)))
+
+(defn- can-connect? [details]
+  (let [connection-spec (connection-details->spec details)]
+    (= 1 (first (vals (first (jdbc/query connection-spec ["select 1"])))))))
 
 (defn- humanize-connection-error-message [message]
   (condp re-matches message
@@ -34,7 +37,7 @@
     #"^Wrong user name or password .*$"
     (driver/connection-error-messages :username-or-password-incorrect)
 
-    #".*" ; default
+    #".*"                                                   ; default
     message))
 
 (defrecord H2TcpDriver []
@@ -45,30 +48,31 @@
                  driver/IDriver
                  (merge (sql/IDriverSQLDefaultsMixin)
                         {
-                          :can-connect?          (constantly true)
-                          :date-interval                     (u/drop-first-arg h2/h2-date-interval)
-                          :details-fields                    (constantly [{:name         "host"
-                                                                           :display-name "Host (localhost)"
-                                                                           :default      "localhost"}
-                                                                          {:name         "port"
-                                                                           :display-name "Port (9092)"
-                                                                           :type         :integer
-                                                                           :default      9092}
-                                                                          {:name         "dbname"
-                                                                           :display-name "Database name"
-                                                                           :placeholder  "x-sql-test"
-                                                                           :required     true}
-                                                                          {:name         "user"
-                                                                           :display-name "Database username (sa)"
-                                                                           :placeholder  "sa"
-                                                                           :required     true}
-                                                                          {:name         "password (sa)"
-                                                                           :display-name "Database password"
-                                                                           :type         :password
-                                                                           :placeholder  "*******"}
-                                                                          ])
-                          :humanize-connection-error-message (u/drop-first-arg humanize-connection-error-message)
-                          :current-db-time                   (driver/make-current-db-time-fn h2/h2-db-time-query h2/h2-date-formatters)})
+                         :can-connect?                      (u/drop-first-arg can-connect?)
+                         :date-interval                     (u/drop-first-arg h2/h2-date-interval)
+                         :details-fields                    (constantly [
+                                                                         ;{:name         "host"
+                                                                         ; :display-name "Host (localhost)"
+                                                                         ; :default      "localhost"}
+                                                                         ;{:name         "port"
+                                                                         ; :display-name "Port (9092)"
+                                                                         ; :type         :integer
+                                                                         ; :default      9092}
+                                                                         {:name         "dbname"
+                                                                          :display-name "Database name"
+                                                                          :placeholder  "xsqltest"
+                                                                          :required     true}
+                                                                         ;{:name         "user"
+                                                                         ; :display-name "Database username (sa)"
+                                                                         ; :placeholder  "sa"
+                                                                         ; :required     true}
+                                                                         ;{:name         "password"
+                                                                         ; :display-name "Database password (sa)"
+                                                                         ; :type         :password
+                                                                         ; :placeholder  "*******"}
+                                                                         ])
+                         :humanize-connection-error-message (u/drop-first-arg humanize-connection-error-message)
+                         :current-db-time                   (driver/make-current-db-time-fn h2/h2-db-time-query h2/h2-date-formatters)})
 
                  sql/ISQLDriver
                  (merge (sql/ISQLDriverDefaultsMixin)
