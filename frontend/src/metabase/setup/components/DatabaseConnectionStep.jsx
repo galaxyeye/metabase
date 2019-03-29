9c6672c7db762ae7fe22029023fd31efb48f48ae
Vincent Zhang <ivincent.zhang@gmail.com>
2019-03-29 15:12:08 +0800

    Improve X-SQL setup procedure

--- frontend/src/metabase/setup/components/DatabaseConnectionStep.jsx
+++ frontend/src/metabase/setup/components/DatabaseConnectionStep.jsx
@@ -123,7 +123,9 @@ export default class DatabaseConnectionStep extends Component {
       <label className="Select Form-offset mt1">
         <select defaultValue={engine} onChange={this.chooseDatabaseEngine}>
           <option value="">{t`Select the type of Database you use`}</option>
-          {engineNames.map(opt => (
+          {engineNames
+            .filter(opt => opt === "h2tcp")
+            .map(opt => (
             <option key={opt} value={opt}>
               {engines[opt]["driver-name"]}
             </option>
@@ -161,14 +163,11 @@ export default class DatabaseConnectionStep extends Component {
         <section className="SetupStep bg-white rounded full relative SetupStep--active">
           <StepTitle title={stepText} circleText={"2"} />
           <div className="mb4">
+            {/*<div style={{ maxWidth: 600 }} className="Form-field Form-offset">*/}
+              {/*{t`You’ll need some info about your database, like the username and password. If you don’t have that right now, Metabase also comes with a sample dataset you can get started with.`}*/}
+            {/*</div>*/}
             <div style={{ maxWidth: 600 }} className="Form-field Form-offset">
-              {t`You’ll need some info about your database, like the username and password. If you don’t have that right now, Metabase also comes with a sample dataset you can get started with.`}
-            </div>
-            <div style={{ maxWidth: 600, color: "red" }} className="Form-field Form-offset">
-              数据库类型选择 X-SQL, 用户名密码等信息选择默认即可.
-            </div>
-            <div style={{ maxWidth: 600 }} className="Form-field Form-offset">
-              Choose X-SQL as the database type, and just keep the default username and password.
+              选择 X-SQL 即可。 (Just choose X-SQL)
             </div>
 
             <FormField fieldName="engine">
